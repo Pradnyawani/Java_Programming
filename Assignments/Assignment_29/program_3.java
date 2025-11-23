f8c6d0e36605940d442e3f56e67a3869002b76a8
Pradnyawani <pradnyawani7@gmail.com>
2025-11-23 19:49:28 +0530

    Add files via upload

---
+++ Assignments/Assignment_29/program_3.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        
+      int i=0,j=0;
+      
+
+      for(i=1;i<=iRow;i++)
+      {
+        
+        for(j=1;j<=iCol;j++)
+        {
+         if(i%2!=0)
+         {
+            char ch=(char)('a'+(j-1));
+            System.out.print(ch+ "\t");
+         }
+           else
+           {
+            System.out.print(j+ "\t");
+           }
+         System.out.println();
+      }
+     
+    }
+}
+}
+
+class program_3
+{
+  public static void main(String A[])
+      {
+        Scanner sobj=new Scanner(System.in);
+        int iValue1=0;
+        int iValue2=0;
+
+        System.out.println("enter the  number of rows:");
+        iValue1=sobj.nextInt();
+
+         System.out.println("enter the number of columns:");
+        iValue2=sobj.nextInt();
+
+        
+
+        Pattern pobj=new Pattern();
+        pobj.Display(iValue1,iValue2);
+
+      }
+}
+//2n
