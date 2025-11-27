cbb066e423fe64c0b0ea62906810856f49fa64c1
Pradnyawani <pradnyawani7@gmail.com>
2025-11-27 12:13:05 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/program_5.java
@@ -0,0 +1,63 @@
+
+
+import java.util.Scanner;
+class Pattern
+{
+    //filter for square matrix
+    public void Display(int iRow,int iCol)
+    {
+        //logic
+        int i=0,j=0;
+        if(iRow !=iCol)
+        {
+            System.out.println("Tnvalid input");
+             System.out.println("row number and column number should be same");
+             return;
+
+        }
+
+        for(i=1;i<=iRow;i++)
+        
+        {
+                 
+            for(j=1;j<=iCol;j++)
+            {
+                if(j>=i)
+                {
+                    System.out.print(j+"\t");
+                }
+                else{
+                    System.out.printf("\t");
+                }
+                
+                
+                
+                
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
+
+class program_5
+{
+    public static void main(String A[])
+    {
+        Scanner sobj=new Scanner(System.in);
+
+        int iValue1=0,iValue2=0;
+
+       
+        System.out.println("enter the number of rows:");
+        iValue1=sobj.nextInt();
+
+        System.out.println("enter the number of columns:");
+        iValue2=sobj.nextInt();
+
+        Pattern pobj=new Pattern();
+        pobj.Display(iValue1,iValue2);
+
+    }
+}
